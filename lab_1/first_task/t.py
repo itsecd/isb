650f2720da6d86e0937e403a42e83014bd94088c
IvaKorsya <korsaeva24@gmail.com>
2024-03-13 22:37:10 +0400

    added encrypt and .gitignore

--- lab_1/first_task/t.py
+++ lab_1/first_task/t.py
@@ -1,23 +1,25 @@
-# with open("input.txt",'r', encoding='utf-8') as file:
-#     lines=file.readlines()
+from work_w_files import write_file, read_file
 
-# new_letters=[]
-# rot=5
-# for line in lines:
-#     for char in line:
-#         if (char>='А' and char<='Я'):
-#             new_letters.append(chr((ord(char)-ord('А')+1+rot)%33+ord('А')))
-#         elif (char>='а' and char<='е'):
-#             new_letters.append(chr((ord(char)-ord('а')+rot+1)%33+ord('а')))
-#         elif (char>'е' and char<='я'):
-#             new_letters.append(chr((ord(char)-ord('а')+rot+2)%33+ord('а')))
-#         else:
-#             new_letters.append(char)
-# print(new_letters)
-letter = 'А'
-while(letter<='я'):
-    print(f'"{letter}"')
-    letter=chr(ord(letter)+1)
-# with open("otus.txt", "w", encoding='utf-8') as file:
-#     for char in new_letters:
-#         file.write(char)
+def encrypt_w_caesar_cipher(text:list[str],shift=5): 
+    new_letters=[]
+    for line in text:
+        line.replace('ё','е')
+        line.replace('Ё','Е')
+
+    for line in text:
+        for char in line:
+            if(char>='а' and char<='я'):
+                new_letters.append(chr((ord(char)+shift-ord('а'))%32+ord('а')))
+            elif(char>='А' and char<='Я'):
+                new_letters.append(chr((ord(char)+shift-ord('А'))%32+ord('А')))
+            else:
+                new_letters.append(char)
+    return new_letters
+
+with open("out.txt",'w', encoding='utf-8') as file:
+    for char in (encrypt_w_caesar_cipher(read_file('input.txt'))):
+        file.write(char)
+
+    
+print(encrypt_w_caesar_cipher(read_file('input.txt')))
+
