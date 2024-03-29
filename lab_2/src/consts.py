ad22849168428e4bcf7022043b666a31aa24e0ab
ni vi <fokin-klock@mail.ru>
2024-03-30 00:04:33 +0400

    asd

--- lab_2/src/consts.py
+++ lab_2/src/consts.py
@@ -1,1 +1,12 @@
-THEORETICAL_PROBABILITIES = (0.2148, 0.3672, 0.2305, 0.1875)
+THEORETICAL_PROBABILITIES = {1 : 0.2148, 2 : 0.3672, 3 : 0.2305, 4 : 0.1875}
+
+def hash(key: int) -> int: 
+    
+    if key >= 4:
+        return 4
+    if key <= 1:
+        return 1
+    return key
+
+MAX_LENGTH_BLOCK = 8
+COUNT_BITS = 128
--- lab_2/src/tests.py
+++ lab_2/src/tests.py
@@ -1,5 +1,9 @@
 import math
 
+import mpmath
+
+from consts import THEORETICAL_PROBABILITIES, MAX_LENGTH_BLOCK, COUNT_BITS, hash
+
 def frequency_bit_test(array_bits: str) -> float:
     
     if not all(char in '01' for char in array_bits):
@@ -7,13 +11,12 @@ def frequency_bit_test(array_bits: str) -> float:
     
     sum = 0
     
-    for bit in array_bits:
-            
+    for bit in array_bits:        
         if bit == '1':
             sum += 1
         elif bit =='0':
             sum += -1
-    
+
     return math.erfc(sum / math.sqrt(len(array_bits)) / math.sqrt(len(2)))
         
 def identical_consecutive_bits_test(array_bits: str) -> float:
@@ -29,16 +32,32 @@ def identical_consecutive_bits_test(array_bits: str) -> float:
     count_alternating_signs = sum(1 if array_bits[i] != array_bits[i + 1] else 0 for i in range(len(array_bits) - 1))
     
     return math.erfc(abs(count_alternating_signs - 2 * len(array_bits) * fraction_ones * (1 - fraction_ones)) / 
-                     (2 * math.sqrt(2 * len(array_bits)) * fraction_ones * (1 - fraction_ones))
-                    )
+                     (2 * math.sqrt(2 * len(array_bits)) * fraction_ones * (1 - fraction_ones)))
 
-def longest_sequence_ones_block_eight_test(array_bits: str) -> float:
+def longest_sequence_ones_block_eight_test(array_bits: str):
     
     if not all(char in '01' for char in array_bits):
         raise ValueError("Это не строка битов")
     
-    if len(array_bits) != 128:
-        raise ValueError("Этот тест работает только с 128 битами")
+    if len(array_bits) != COUNT_BITS:
+        raise ValueError(f"Этот тест работает только с {COUNT_BITS} битами")
+        
+    blocks_info = {key: 0 for key in THEORETICAL_PROBABILITIES.keys()}
+
+    for step in range(0, len(array_bits), MAX_LENGTH_BLOCK):
     
+        block = array_bits[step: step + MAX_LENGTH_BLOCK]    
+        max_length, length = 0, 0
+        
+        for bit in block:
+            
+            length = length + 1 if bit == '1' else 0
+            max_length = max(max_length, length)
     
-    
+        blocks_info[hash(max_length)] += 1
+        
+    hi_square = 0
+    for key in blocks_info.keys():
+        hi_square += math.pow(blocks_info[key] - 16 * THEORETICAL_PROBABILITIES[key], 2) / (16 * THEORETICAL_PROBABILITIES[key])
+                
+    return mpmath.gammainc(1.5, hi_square / 2)
