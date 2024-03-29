a4f8422fd81a2a0a7910735ca7ef7c26886d53a7
karma <126313574+nick-vivo@users.noreply.github.com>
2024-03-30 02:36:54 +0400

    Update solution.py

--- lab_2/solution.py
+++ lab_2/solution.py
@@ -24,13 +24,10 @@ def main():
 
     try:
         for type_generate in HEADERS_GENERATE:
-
             path = os.path.join(NAME_INPUT_FILE)
-
             data = json_reader.read_json(path)
 
             path_generate = os.path.join(data[HEADER_FOLDER_RESOURCE], data[type_generate])
-
             with open(path_generate, 'r') as f:
                 generate = f.read()
 
