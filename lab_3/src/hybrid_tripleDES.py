c1cf3475c901bf43eb656cb90552f5623e1dbdad
ni vi <fokin-klock@mail.ru>
2024-04-02 03:13:26 +0400

    asdas

--- lab_3/src/hybrid_tripleDES.py
+++ lab_3/src/hybrid_tripleDES.py
@@ -49,15 +49,16 @@ def generate_hybrid_tripleDes(
             key = SymmetricTripleDES.generate_key()
 
         private_key = asymmetric.generate_private_key(
-            key_size=len_rsa_private_key)
+                            key_size=len_rsa_private_key)
         encrypt_key = asymmetric.encrypt_with_public_key(
-            key, private_key.public_key())
+                            key, 
+                            private_key.public_key())
 
         logger.info("Generate hybrid tripleDes successful")
         return (SymmetricTripleDES(
                     stored_key=encrypt_key, 
                     init_vector=SymmetricTripleDES.generate_init_vector()),
-                private_key)
+                    private_key)
 
     except Exception as e:
         logger.error(f"Generate hybrid tripleDes error: {e}")
@@ -94,18 +95,20 @@ def encrypt_text(text: bytes,
                             "argument BYTE")
         
         original_symmetric_key = asymmetric.decrypt(
-            symmetric_encrypt_key.stored_key,
-            asymetric_key)
+                                    symmetric_encrypt_key.stored_key,
+                                    asymetric_key)
 
         if len_block_padding:
             encrypted_text = symmetric_encrypt_key.encrypt(
-                text, original_symmetric_key, len_block_padding
+                                text, 
+                                original_symmetric_key, 
+                                len_block_padding
             )
         else:
             encrypted_text = symmetric_encrypt_key.encrypt(
-                text, 
-                original_symmetric_key, 
-                type_len_block_padding=type_len_block_padding
+                                text, 
+                                original_symmetric_key, 
+                                type_len_block_padding=type_len_block_padding
             )
         logger.info("Encrypt text into hybrid tripleDes successful")
         return encrypted_text
