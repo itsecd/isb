f6fcdc81751547adcf83aa046c6a3aad4d389ade
Григорий Петров <petrovgrigorij29@gmail.com>
2024-05-28 11:44:03 +0400

    fix all issues

--- lab_1/task2/decrypt.py
+++ lab_1/task2/decrypt.py
@@ -4,8 +4,6 @@ import sys
 
 from collections import Counter
 
-from constants import ENCRYPTED_LETTERS, PATHS
-
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
@@ -26,26 +24,29 @@ def frequency(enc_text: str) -> dict:
     return {tup[0]: tup[1] / sum(tup[1] for tup in dict_pairs) for tup in dict_pairs}
 
 
-def decrypt_text(text_for_decrypt: str, arr_decrypt_letters: dict) -> str:
+def decrypt_text(enc_letters: list, text_for_decrypt: str, arr_decrypt_letters: dict) -> str:
     """
     Decrypt text using frequency analysis algorithm
 
+    :param enc_letters: List of letters by frequency in the decrypted text
     :param text_for_decrypt:  input encrypted text
     :param arr_decrypt_letters: list of letters in descending order of frequency in encrypted text
     :return: decrypted text using algorithm frequency analysis
     """
     arr_encrypt_text = []
 
-    dictionary = dict(zip(list(arr_decrypt_letters.keys()), ENCRYPTED_LETTERS))
+    dictionary = dict(zip(list(arr_decrypt_letters.keys()), enc_letters))
     for symb in text_for_decrypt:
         arr_encrypt_text.append(dictionary[symb])
     text_for_decrypt = ''.join(arr_encrypt_text)
     return text_for_decrypt
 
 
-def write_result(path_decrypt: str, path_key: str, path_input: str) -> None:
+def write_result(enc_letters: list, path_decrypt: str, path_key: str, path_input: str) -> None:
     """
     Write decrypted text and keys in file
+
+    :param enc_letters: List of letters by frequency in the decrypted text
     :param path_input: path of file with input encrypted text
     :param path_key: path of file with key
     :param path_decrypt: path of file with decrypted text
@@ -54,7 +55,7 @@ def write_result(path_decrypt: str, path_key: str, path_input: str) -> None:
     try:
         txt_writer(path_decrypt, decrypt_text(txt_reader(path_input), frequency(txt_reader(path_input))))
 
-        keys = dict(zip(list(frequency(txt_reader(path_input))), ENCRYPTED_LETTERS))
+        keys = dict(zip(list(frequency(txt_reader(path_input))), enc_letters))
 
         json_writer(path_key, keys)
     except Exception as ex:
@@ -62,9 +63,11 @@ def write_result(path_decrypt: str, path_key: str, path_input: str) -> None:
 
 
 if __name__ == "__main__":
-    paths = json_reader(PATHS)
+    encrypted_letters: list = json_reader("constants.json")["encrypted_letters"]
+
+    paths = json_reader(json_reader("constants.json")["paths"])
     try:
-        write_result(os.path.join(paths["folder"], paths["decrypt"]),
+        write_result(encrypted_letters, os.path.join(paths["folder"], paths["decrypt"]),
                      os.path.join(paths["folder"], paths["key"]), os.path.join(paths["folder"], paths["input"]))
         logging.info(f"Text successfully decrypted and saved to file")
     except Exception as ex:
