6808f4f6f1da71312852c480eb9206a3cc2235af
илья <Oksimonov@mail.ru>
2025-03-06 23:33:21 +0300

    начало нового шифра

--- lab_1/Task_2/main.py
+++ lab_1/Task_2/main.py
@@ -1,17 +1,23 @@
-task_text = ("КwЧ5Д>ЫХЧ1ЪЕt Й2>ХИЬЧЙ ФХ 1 ХБЧБХЫПЫХЪ"
-             " ЕЩЕtФЙХБЕ2rtЫИИ ХrЕЯЩЕ1ФУЙХДЫХЙЕ17БЕХ8ЛЛЫБЙ" 
-             " ЩДЕХМtЧД Й7ХБЕДЛ wЫД4 Ч17Д>ЫХwЧДД>ЫХДЕХ ХДЧrt" 
-             " 2ЫtХЯДЧ5 ЙЫ17ДЕХК2ЫД7О" 
-             " Й7ХtЧЯ2ЫtХrtЕЪtЧ22>ХБЕЙЕtКУХЯЧХЕw ДХrt" 
-             " Ы2Х2ЕЬДЕХЯЧЪtКЯ Й7ХЩХrЧ2ФЙ7Х Х ИrЕ1Д" 
-             " Й7ХИКПЫИЙЩКЫЙХДЫХ2ЫД7ОЫХwУЬ Д>ХtЧЯ1" 
-             " 5Д>МХКrЧБЕЩП БЕЩХ ИrЕ1Д 2>МХЛЧА1ЕЩХДЫБЕЙЕt>ЫХ" 
-             " ЯХД МХИЕwЫtЬЧЙХtЫЧ1 ЯЧ4 ХЧ1ЪЕt Й2ЕЩХО ЛtЕЩЧД" 
-             " ФХИХ4Ы17УХКИ1ЕЬД Й7ХЬ ЯД7ХrЕЙЫД4" 
-             " Ч17Д>2ХМЧБЫtЧ2ХДЕХД ХЩХЕwДЕАХ ЯХД МХЧ1ЪЕt Й2ХО" 
-             " ЛtЕЩЧД ФХД БЧБХДЫХИЩФЯЧДХИХЧ1ЪЕt Й2Е2ХИЬЧЙ ФХЕД" 
-             " ХtЫЧ1 ЯЕЩЧД>ХБЧБХЕЙwЫ17Д>ЫХЧ1ЪЕt Й2>Х Х" 
-             " ИrЕ17ЯКУЙИФХtЧЯwЫ17ДЕ")
+task_text = ("И7уОПЁХХQU=Ё<Ж7ПUNRО9уQПNОИ7ЖЁХЯQUПДОИ=ЁХQЖQЖЁQО"
+            "уТДО=ЯПiЁZ=71РЁОU7%7ОYQОПЯХ7%7ОРТR<ЯОР7U7=КХОЙКТ7О9ЯiЁZ"
+            "=71ЯЖ7ОИ7ПТЯЖЁQ"
+            "7ПЖ71ЖКQОU=QЙ71ЯЖЁДОРОПЁХХQU=Ё<ЖКХОЯТ%7=ЁUХЯХОUЯР"
+            "ЁQОПUЯUЁПUЁ<QПРЁюО9ЯР7Ж7ХQ=Ж7ПUQ3О1ОiЁZ=sQХ7ХОП77ЙF"
+            "QЖЁЁО7ПUЯ1ЯUNПДОЖQОу7ТYЖ7ОТЁЖQ3Ж7ПUЁОU7YQ"
+            "ПЁХХQU=Ё<ЖКQОПЁПUQХКО1ОП17RО7<Q=QуNОуQТДUОЖЯОЙТ7<ЖКQ"
+            "ОЁОИ7U7<ЖКQ"
+            "О1ОЙТ7<ЖКюОПЁПUQХЯюОiЁZ=71ЯЖЁQО1КИ7ТЖДQUПДОUЯРОЁПю"
+            "7уЖ7QОП77ЙFQЖЁQОуQТЁUПДОЖЯОЙТ7РЁОЯО9ЯUQХОРЯYуК3ОЁ9"
+            "ОЖЁюОР7уЁ=sQUПДОПОЁПИ7ТN971ЯЖЁQХО7И=QуQТQЖЖ7%7ОРТR<Я"
+            "1ОИ7U7<ЖКюОЯТ%7=ЁUХЯюОZ7=ХЁ=sQUПДОUЯРОЖЯ9К1ЯQХЯДО1"
+            "Кю7уЖЯДО%ЯХХЯООЁО1ОИ=7гQППQОQQО%QЖQ=Ё=71ЯЖЁДО7ПsF"
+            "QПU1ТДQUПДОiЁZ=71ЯЖЁQОИ7ПТЯЖЁД"
+            "U7ОQПUNО7ЖЯОИ7U7Р7ХОЖЯРТЯуК1ЯQUПДОЖЯОЁПю7уЖЁР"
+            "1Ою7уQОПЁХХQU=Ё<Ж7%7ОiЁZ=71ЯЖЁДОуТДОИ7уПUЯЖ71РЁОЁОИ"
+            "Q=QПUЯЖ71РЁОЁПю7уЖКюОуЯЖЖКюО9ЯуQ3ПU1sRUПДОПТ7YЖКQ"
+            "ОХЖ7%7s=71ЖQ1КQОЯТ%7=ЁUХК"
+            "GUЁюОs=71ЖQ3ООХ7YQUОЙКUNО7<QЖNОХЖ7%7ОЁОРЯYуК3ОП7О"
+            "П17ЁХОРТR<7Х")
 
 
 rus_freq = {
@@ -79,32 +85,34 @@ def main():
     print("\nЗашифрованный текст:\n")
     print(text)
 
-    #Свод к единому алфавиту для удобства
-    for i in range(len(text)):
-        if text[i] == "8":
-            text = text.replace("8", "В")
-        elif text[i] == "5":
-            text = text.replace("5", "Г")
-        elif text[i] == "4":
-            text = text.replace("4", "Ю")
-        elif text[i] == "7":
-            text = text.replace("7", "Ж")
-        elif text[i] == "2":
-            text = text.replace("2", "З")
-        elif text[i] == "1":
-            text = text.replace("1", "Н")
-        elif text[i] == "w":
-            text = text.replace("w", "С")
-        elif text[i] == "r":
-            text = text.replace("r", "Т")
-        elif text[i] == "t":
-            text = text.replace("t", "Ц")
-        elif text[i] == ">":
-            text = text.replace(">", "Э")
-    print("______________________________________________________________\n\nСвод к единому алфавиту:\n")
-    print(text)
+    # #Свод к единому алфавиту для удобства
+    # for i in range(len(text)):
+    #     if text[i] == "8":
+    #         text = text.replace("8", "В")
+    #     elif text[i] == "5":
+    #         text = text.replace("5", "Г")
+    #     elif text[i] == "4":
+    #         text = text.replace("4", "Ю")
+    #     elif text[i] == "7":
+    #         text = text.replace("7", "Ж")
+    #     elif text[i] == "2":
+    #         text = text.replace("2", "З")
+    #     elif text[i] == "1":
+    #         text = text.replace("1", "Н")
+    #     elif text[i] == "w":
+    #         text = text.replace("w", "С")
+    #     elif text[i] == "r":
+    #         text = text.replace("r", "Т")
+    #     elif text[i] == "t":
+    #         text = text.replace("t", "Ц")
+    #     elif text[i] == ">":
+    #         text = text.replace(">", "Э")
+    # print("______________________________________________________________\n\nСвод к единому алфавиту:\n")
+    # print(text)
+
 
     #Процентное соотношения
+
     percent_dict = calculate_char_percentages(text)
 
     #Сортировка значений ключей
@@ -115,80 +123,83 @@ def main():
     print(sorted_dict)
 
     print("\n______________________________________________________________\n\nДешифрованный текст:\n")
-    text = replace_chars(text, " ", "и") #
-    text = replace_chars(text, "Е", "о") #
-    text = replace_chars(text, "Д", "н") #
-    text = replace_chars(text, "Ч", "а") #
 
-    text = replace_chars(text, "Ы", "е") #
-    text = replace_chars(text, "Ц", "р")
-    text = replace_chars(text, "Й", "т")
-    text = replace_chars(text, "Н", "л")
 
-    text = replace_chars(text, "З", "м")
-    text = replace_chars(text, "Ж", "ь")
-    text = replace_chars(text, "И", "с")
-    text = replace_chars(text, "Б", "к") #
+    # text = replace_chars(text, " ", "и") #
+    # text = replace_chars(text, "Е", "о") #
+    # text = replace_chars(text, "Д", "н") #
+    # text = replace_chars(text, "Ч", "а") #
+    #
+    # text = replace_chars(text, "Ы", "е") #
+    # text = replace_chars(text, "Ц", "р")
+    # text = replace_chars(text, "Й", "т")
+    # text = replace_chars(text, "Н", "л")
+    #
+    # text = replace_chars(text, "З", "м")
+    # text = replace_chars(text, "Ж", "ь")
+    # text = replace_chars(text, "И", "с")
+    # text = replace_chars(text, "Б", "к") #
+    #
+    # text = replace_chars(text, "Я", "з")
+    # text = replace_chars(text, "Щ", "в")
+    # text = replace_chars(text, "Э", "ы")
+    # text = replace_chars(text, "Т", "п")
+    #
+    # text = replace_chars(text, "К", "у")
+    # text = replace_chars(text, "С", "д")
+    # text = replace_chars(text, "Ф", "я")
+    # text = replace_chars(text, "Ъ", "г")
+    #
+    # text = replace_chars(text, "Ь", "ж")
+    # text = replace_chars(text, "Л", "ф")
+    # text = replace_chars(text, "М", "х")
+    # text = replace_chars(text, "У", "ю")
+    #
+    # text = replace_chars(text, "Ю", "ц")
+    # text = replace_chars(text, "О", "ш")
+    # text = replace_chars(text, "Г", "ч")
+    # text = replace_chars(text, "П", "щ")
+    #
+    # text = replace_chars(text, "А", "й")
+    # text = replace_chars(text, "В", "э")
+    #
+    # #Замена в конце дабы исключить повторения символа " " в алфавите
+    text = replace_chars(text, "О", " ")
 
-    text = replace_chars(text, "Я", "з")
-    text = replace_chars(text, "Щ", "в")
-    text = replace_chars(text, "Э", "ы")
-    text = replace_chars(text, "Т", "п")
 
-    text = replace_chars(text, "К", "у")
-    text = replace_chars(text, "С", "д")
-    text = replace_chars(text, "Ф", "я")
-    text = replace_chars(text, "Ъ", "г")
-
-    text = replace_chars(text, "Ь", "ж")
-    text = replace_chars(text, "Л", "ф")
-    text = replace_chars(text, "М", "х")
-    text = replace_chars(text, "У", "ю")
-
-    text = replace_chars(text, "Ю", "ц")
-    text = replace_chars(text, "О", "ш")
-    text = replace_chars(text, "Г", "ч")
-    text = replace_chars(text, "П", "щ")
-
-    text = replace_chars(text, "А", "й")
-    text = replace_chars(text, "В", "э")
-
-    #Замена в конце дабы исключить повторения символа " " в алфавите
-    text = replace_chars(text, "Х", " ")
-
-    crypt_key = {
-        "r": "п",
-        "w": "с",
-        "t": "р",
-
-        "1": "л",
-        "2": "м",
-        "4": "ц",
-        "5": "ч",
-        "7": "ь",
-        "8": "э",
-
-        "А": "й",
-        "Б": "к",
-        "Д": "н",
-        "Е": "о",
-        "И": "с",
-        "Й": "т",
-        "К": "у",
-        "Л": "ф",
-        "М": "х",
-        "О": "ш",
-        "П": "щ",
-        "У": "ю",
-        "Ф": "я",
-        "Х": " ",
-        "Ч": "а",
-        "Щ": "в",
-        "Ъ": "г",
-        "Ы": "е",
-        "Ь": "ж",
-        "Я": "з",
-    }
+    # crypt_key = {
+    #     "r": "п",
+    #     "w": "с",
+    #     "t": "р",
+    #
+    #     "1": "л",
+    #     "2": "м",
+    #     "4": "ц",
+    #     "5": "ч",
+    #     "7": "ь",
+    #     "8": "э",
+    #
+    #     "А": "й",
+    #     "Б": "к",
+    #     "Д": "н",
+    #     "Е": "о",
+    #     "И": "с",
+    #     "Й": "т",
+    #     "К": "у",
+    #     "Л": "ф",
+    #     "М": "х",
+    #     "О": "ш",
+    #     "П": "щ",
+    #     "У": "ю",
+    #     "Ф": "я",
+    #     "Х": " ",
+    #     "Ч": "а",
+    #     "Щ": "в",
+    #     "Ъ": "г",
+    #     "Ы": "е",
+    #     "Ь": "ж",
+    #     "Я": "з",
+    # }
 
     print(text)
     print("\n")
@@ -196,7 +207,7 @@ def main():
     #Запись в файлы
     write_to_file("encrypted_text.txt", task_text)
     write_to_file("decrypted_text.txt", text)
-    write_to_file("decrypt_key.txt", str(crypt_key))
+    #write_to_file("decrypt_key.txt", str(crypt_key))
 
 if __name__ == "__main__":
     main()
