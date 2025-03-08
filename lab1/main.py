2f27294f142230662af3bc2faf1b92be07f99005
catchtimings <chindina.63@gmail.com>
2025-03-08 13:19:23 +0400

    [~] check functions

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,12 @@
 from file_work import *
+from task1.text_encryption import text_encryption
 
 
 def main():
     text = read_from_file("task1/text.txt")
-    write_to_file("new_text.txt", "Hello")
+    key = read_key("task1/key.json")
+    new_text = text_encryption(text, key)
+    write_to_file("encrypted_text.txt", new_text)
 
 
 if __name__ == "__main__":
