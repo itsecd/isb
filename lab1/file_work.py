398417f165a333e42c411ff25a99034ca39ffae9
catchtimings <chindina.63@gmail.com>
2025-03-08 15:42:21 +0400

    [+] save_data function

--- lab1/file_work.py
+++ lab1/file_work.py
@@ -22,7 +22,7 @@ def write_to_file(directory: str, data: str) -> None:
         file.write(data)
 
 
-def read_key(directory: str) -> dict[str, int]:
+def read_data(directory: str) -> dict[str, int]:
     """
     The function opens a json file and reads the key as a dictionary
     :param directory: json file directory
@@ -30,3 +30,14 @@ def read_key(directory: str) -> dict[str, int]:
     """
     with open(directory, mode="r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def save_data(directory: str, data: dict[str, float]) -> None:
+    """
+    The function saves the data to a json file
+    :param directory: json file directory
+    :param data: data that will be written to the file
+    :return:
+    """
+    with open(directory, mode="w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False)
