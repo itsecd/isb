52edf0083e5c75b62228d821af3608b614bcc531
Kirill <kirillstengauer@gmail.com>
2025-03-14 18:33:53 +0400

    В первом задании выполнил шифровку текста методом Тритемиуса, во втором расшифровал текст методом частотного анализа. Для запуска необходимо в командной строке прописать python main.py 'ключ для задания1' 'текст для задания 1' 'файл куда будет сохранен результат выполнения задания1' 'ключ для задания2' 'текс для задания2' 'файл куда будет сохранен результат выполнения задания2' (Переделал для своего варианта)

---
+++ functions.py
@@ -0,0 +1,88 @@
+from collections import Counter
+import json
+
+def read_file(filename: str)->str:
+    """
+    Opens a file in read mode
+    :param filename: directory of the file
+    :return: content of the file as a string
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def write_file(filename: str, data:str)->int:
+    """
+    Opens a file in write mode
+    :param filename: directory of the file
+    :param data: text that will be added to the file
+    :return: number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(data)
+
+def tritemius_cipher(text: str, key: str)->str:
+    """
+    Gets encrypted text using Trithemius Cipher
+    :param text: directory to original text
+    :param key: word that will be using for encryption
+    :return: encrypted text as a string
+    """
+    alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789"
+    char_to_num = {char: i + 1 for i, char in enumerate(alphabet)}
+    num_to_char = {i + 1: char for i, char in enumerate(alphabet)}
+
+    text = text.upper()
+    key = key.upper()
+
+    encrypted_text = []
+    key_id = 0
+
+    for i, char in enumerate(text):
+        if char in char_to_num:
+            text_num = char_to_num[char]
+            key_char = key[key_id % len(key)]
+            key_num = char_to_num[key_char]
+
+            encrypted_num = text_num + key_num
+            if encrypted_num > len(alphabet):
+                encrypted_num -= len(alphabet)
+            encrypted_text.append(num_to_char[encrypted_num])
+
+            key_id+=1
+        else:
+            encrypted_text.append(char)
+
+    return ''.join(encrypted_text)
+
+def get_frequency(filename: str)->None:
+    """
+    Gets a frequency for every character in the text
+    :param filename: directory to the text file
+    """
+    char_count = Counter(filename)
+    total_chars = len(filename)
+    char_frequency = {char: count/total_chars for char, count in char_count.items() }
+    sorted_char_frequency = dict(sorted(char_frequency.items(), key=lambda item: item[1], reverse=True))
+    with open("frequency_analysis.json", 'w', encoding='utf-8') as file:
+        json.dump(sorted_char_frequency, file, ensure_ascii=False, indent=4)
+
+def get_key(filename: str)->dict:
+    """
+    Gets a key for decryption the text
+    :param filename: directory of the key file
+    :return: key for decryption
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        key=json.load(file)
+    reverse_key = {char: k for k, char in key.items()}
+    return reverse_key
+
+def get_decryption(encrypted_text: str, key: dict)->str:
+    """
+    Gets decrypted text
+    :param encrypted_text: directory to encrypted text
+    :param key: key for decryption
+    :return: decrypted text as a string
+    """
+    decrypted_text = ''.join([key.get(char,char) for char in encrypted_text])
+    return decrypted_text
---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import functions
+
+def create_parser()->tuple:
+    """
+    Gets a path to key, decrypted and encrypted texts for task1 and key, encrypted and decrypted texts for task2
+    :return: tuple of 6 strings
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("key",type=str,help="key")
+    parser.add_argument("input_text",type=str,help="path to the original text")
+    parser.add_argument("encrypted_text", type=str, help="path where result for task1 will be saved")
+    parser.add_argument("task2_key", type=str, help="path to the key for task2")
+    parser.add_argument("task2_encrypted", type=str, help="path to encrypted text for task2")
+    parser.add_argument("task2_result", type=str, help="path where result for task2 will be saved")
+    args=parser.parse_args()
+    return args.key, args.input_text, args.encrypted_text, args.task2_key, args.task2_encrypted, args.task2_result
+
+def main():
+    try:
+        key = functions.read_file(create_parser()[0])
+        text = functions.read_file(create_parser()[1])
+        result = create_parser()[2]
+
+        encrypted_text = functions.tritemius_cipher(text, key)
+        functions.write_file(result, encrypted_text)
+
+        task2_key = create_parser()[3]
+        original_text_task2 = functions.read_file(create_parser()[4])
+        functions.get_frequency(original_text_task2)
+
+        task2_decrypted = create_parser()[5]
+
+        task2_res = functions.get_decryption(original_text_task2, functions.get_key(task2_key))
+        functions.write_file(task2_decrypted, task2_res)
+    except Exception as e:
+        print(f"An error occurred while accessing the directory: {e} ")
+
+
+if __name__ == "__main__":
+    main()
